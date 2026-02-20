0f3613949053f474523cd6581c4bf6ab0f065dca
jShubh-AD <shubhjain798@gmail.com>
2026-02-20 12:52:24 +0530

    feat: added work flow comments

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+
+// connection establish and socket created
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
+
+  // check connection data if driver create new trip/ room if not join existing trio
+  // driver send location to all passengers
+  socket.emit("driver-location", (data) => {
+    console.log("Latitude:", data.lat);
+    console.log("Longitude:", data.lng);
+  });
+
+  // passenger receive location from driver
+  socket.on("passenger-location", (data) => {
+    console.log("Latitude:", data.lat);
+    console.log("Longitude:", data.lng);
+  });
+
 // frotend data
   socket.on("send-location", (data) => {
     console.log("Latitude:", data.lat);
     console.log("Longitude:", data.lng);
   });
 
+  // socket disconnection
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
   });
